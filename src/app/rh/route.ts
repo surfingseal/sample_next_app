647cc3cf807aa455f2ce6abd105a73deffcaf263
Surfing seal <102800012+surfingseal@users.noreply.github.com>
2025-11-05 15:03:40 +0900

    modified:   data.txt 	new file:   src/app/login/page.tsx 	modified:   src/app/rh/route.ts

--- src/app/rh/route.ts
+++ src/app/rh/route.ts
@@ -1,5 +1,6 @@
 "use server";
 import fs from 'fs';
+import { NextResponse } from 'next/server';
 
 const path = './data.txt';
 
@@ -7,22 +8,20 @@ export async function GET(request: Request) {
     const content = fs.readFileSync(path, {flag:'a+'})
         .toString().trim();
 
-    return new Response(JSON.stringify({content:content}), {
+    return new Response(JSON.stringify({content:content.toString()}), {
         status: 200,
         headers: {'Content-Type': 'application/json'},
     });
 }
 
 export async function POST(request: Request) {
-    const body = await request.json();
-
-    fs.appendFileSync(path, body.content + "\n");
-    return new Response(
-        JSON.stringify({content: 'ok'}),
-        {
-            status: 200,
-            headers: {'Content-Type': 'application/json'},
-        }
-    );
+    void request;
+    const formData = await request.formData();
+    const name = formData.get('name');
+    const pass = formData.get('pass');
+    const content = "NAME: " + name + "\n" +
+        "PASS: " + pass;
+    fs.writeFileSync(path, content);
+    return NextResponse.json({ status: 'ok', message: '성공적으로 처리되었습니다.' });
 }
 
